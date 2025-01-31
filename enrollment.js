989851303520731debe8f0a7608865a026dc7527
rangeropbe <samuel82.ward.1553@gmail.com>
2025-01-31 23:29:27 +0530

    Delete enrollment.js

--- enrollment.js
+++
@@ -1,7 +0,0 @@
-function selectCourse(name, price) {
-    if(!currentUser) {
-        document.getElementById('authButtons').style.display = 'flex';
-        return;
-    }
-    showCourseDetails(name, price);
-}
